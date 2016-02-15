cc832102f0185f5df9710c22f959e6c1569acbae
rgueye <ramatoulaye.gueye@ism.edu.sn>
2016-02-15 12:42:07 -0500

    configuration Database

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@ define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
 define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'mabase01');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'gueye');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'secure');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
