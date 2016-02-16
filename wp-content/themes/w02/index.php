38822529911cb006f120298562d9ef0b6c4b2076
rgueye <ramatoulaye.gueye@ism.edu.sn>
2016-02-16 12:18:42 -0500

    wp-content/themes/w02/index.php

--- wp-content/themes/w02/index.php
+++ wp-content/themes/w02/index.php
@@ -1,9 +1,19 @@
 <?php get_header(); ?>
-    <section>
+  <section>
         <!--    Contenu des derniers articles -->
+<?php while (have_posts()): the_post();?>
+	<article>
+	<h2><?php the_title();?></h2>
+	<p><?php the_excerpt();?></p>
+        <p>Article ecrit par<?php the_author();?> en date de<?php the_date()?></p>
+        <p><?php the_date('');?></p>
+	 <a href = "<?php the_permalink(); ?>" class="more">Lire la suite</a>
+        <p><?php comments_popup_link('Aucun commentaire','1 commentaire', '% commentaires');?></p>
+       <?php if (has_post_thumbnail())   {?>
+        <p><?php the_post_thumbnail('thumbnail');?></p>
+        <?php  }?>
+    </article>
+	<?php endwhile;?>
+     
     </section>
-<?php get_footer(); ?><?php get_header(); ?>
-    <section>
-        <!--    Contenu des derniers articles -->
-    </section>
-<?php get_footer(); ?>
+<?php get_footer();?>
